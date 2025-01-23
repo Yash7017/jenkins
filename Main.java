0cb354b154c2160a5f2b5cc0fd18c3085683c3f0
Yash Garg <109918499+Yash7017@users.noreply.github.com>
2025-01-23 15:02:55 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,6 @@ import java.util.*;
 public class Main{
     public static void main(String[] args){
         System.out.println("this is java program");
-        System.out.println("output from my first jenkins program");
-        System.out.println("Edit one more file");
-        System.out.println("Edit second file");
+        System.out.println("after change");
     }
 }
